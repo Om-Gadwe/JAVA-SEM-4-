c247b9fc1a6e80b8035522076840fcdf6ee08212
Om-Gadwe <ogadwe@gmail.com>
2026-01-16 15:27:27 +0530

    hw1

---
+++ assignments/Homework/hw1.java
@@ -0,0 +1,14 @@
+package assignments.Homework;
+
+public class hw1 {
+    public static void main(String[] args) {
+
+        String name = "Om Gadwe";
+        int rollNo = 125;
+        String address = "Pune-Maharashtra";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Address: " + address);
+    }
+}
