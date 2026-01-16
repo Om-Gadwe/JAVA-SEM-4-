ee09910318d6830891860f4fbdf84cd43a3fb03b
Om-Gadwe <ogadwe@gmail.com>
2026-01-16 15:48:33 +0530

    array reversal

---
+++ assignments/FS2.java
@@ -0,0 +1,12 @@
+
+package assignments;
+public class FS2 {
+    public static void main(String[] args){
+        char[] om = {'o','m','G','A','D','W','E'};
+        System.out.println(om);
+        for(int i = om.length-1 ; i>=0 ; i--){
+            System.out.print(om[i]);
+        }
+    }
+
+}
