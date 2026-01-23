9ff5769ff8fa4eb75d9233280b4574d3240a13bb
Om-Gadwe <ogadwe@gmail.com>
2026-01-23 13:12:06 +0530

    Parameterized constructor: Print Name, Roll No, PRN (Scanner input)

---
+++ assignments/Homework/HW2.java
@@ -0,0 +1,33 @@
+package assignments.Homework;
+import java.util.Scanner;
+class UserInfo {
+    String nm;
+    int rn;
+    String prn;
+    UserInfo(String nm, int rn, String prn) {
+        this.nm = nm;
+        this.rn = rn;
+        this.prn = prn;
+    }
+    void show() {
+        System.out.println("Name: " + nm);
+        System.out.println("Roll No: " + rn);
+        System.out.println("PRN: " + prn);
+    }
+}
+public class HW2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String nm = sc.nextLine();
+        System.out.print("Enter roll no: ");
+        int rn = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter PRN: ");
+        String prn = sc.nextLine();
+        UserInfo obj = new UserInfo(nm, rn, prn);
+        obj.show();
+
+        sc.close();
+    }
+}
