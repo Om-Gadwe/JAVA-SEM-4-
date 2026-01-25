442e6819e7404ed7397faf4fbced90b6f6c3110d
Om-Gadwe <ogadwe@gmail.com>
2026-01-25 23:27:13 +0530

    parameterized method and Three different objects accessing variables

---
+++ assignments/Homework/hw6.java
@@ -0,0 +1,61 @@
+package assignments.Homework;
+public class hw6 {
+    //Parameterized method
+    /*
+    static void showData(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+    public static void main(String[] args) {
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        showData(name, age);
+    }
+    */
+    //Three different objects accessing variables
+    /*
+    public static void main(String[] args) {
+
+        Student s = new Student("Om", 101);
+        College c = new College("SIU");
+        Course cr = new Course("CSE");
+
+        s.display();
+        c.display();
+        cr.display();
+    }
+    */
+}
+class Student {
+    String name;
+    int roll;
+    Student(String n, int r) {
+        name = n;
+        roll = r;
+    }
+    void display() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll No: " + roll);
+    }
+}
+class College {
+    String collegeName;
+    College(String c) {
+        collegeName = c;
+    }
+    void display() {
+        System.out.println("College: " + collegeName);
+    }
+}
+class Course {
+    String courseName;
+    Course(String c) {
+        courseName = c;
+    }
+    void display() {
+        System.out.println("Course: " + courseName);
+    }
+}
+
+
