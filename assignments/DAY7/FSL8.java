93d990fa104993b0fdd8cf5085df6cc1ee4a819d
Om-Gadwe <ogadwe@gmail.com>
2026-02-07 15:23:49 +0530

    GARBAGE COLLECTION IN JAVA AND ARRAY LIST

---
+++ assignments/DAY7/FSL8.java
@@ -0,0 +1,31 @@
+package assignments.DAY7;
+/* 
+import java.util.ArrayList;
+public class FSL8 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        list.add("Java");
+        list.add("Java");
+        list.add("Programming");
+        System.out.println("Original List: " + list);
+        // Remove the element at index 2
+        String removedElement = list.remove(2);
+        System.out.println("Removed Element: " + removedElement);
+        System.out.println("Updated List: " + list);
+        System.out.println("Size of the list: " + list.size());
+        System.out.println("contain hello " + list.contains("Hello"));
+    }
+    
+}
+*/
+/* 
+class FSL8 {
+    public static void main(String[] args) {
+        FSL8 obj = new FSL8();
+        obj = null; // Nullifying the reference to the object
+        System.gc(); // Suggesting the JVM to perform garbage collection
+    }
+}
+*/
