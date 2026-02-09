42726d0599a650d422a82a8c913b936c576e49ca
Om-Gadwe <ogadwe@gmail.com>
2026-02-10 00:03:08 +0530

    ok

---
+++ assignments/DAY8/FL8.java
@@ -0,0 +1,5 @@
+package assignments.DAY8;
+//Checked, Unchecked Exceptions, Errors, try - catch, finally, throw, throws, user defined exceptions
+public class FL8 {
+    
+}
---
+++ assignments/Homework/DAY7/hw9.java
@@ -0,0 +1,5 @@
+package assignments.Homework.DAY7;
+//SCENARIO BASED QUESTIONS
+public class hw9 {
+    
+}
